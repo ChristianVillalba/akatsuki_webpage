8cc723fa119a4df75c6e193be4f357626575ed38
Christian Villalba UK <christianvillalba.uk@gmail.com>
2024-03-20 23:18:27 +0000

    sindleService component created

---
+++ akatsuki/src/components/Common/SingleService.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const SingleService = (props) => {
+    const { icon, title, description } = props; 
+
+  return (
+    <div className="row text-center">
+      <div className="col-md-4">
+        <span className="fa-stack fa-4x">
+          <i className="fas fa-circle fa-stack-2x text-primary"></i>
+          <i className="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
+        </span>
+        <h4 className="my-3">E-Commerce</h4>
+        <p className="text-muted">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
+          maxime quam architecto quo inventore harum ex magni, dicta impedit.
+        </p>
+      </div>
+      <div className="col-md-4">
+        <span className="fa-stack fa-4x">
+          <i className="fas fa-circle fa-stack-2x text-primary"></i>
+          <i className="fas fa-laptop fa-stack-1x fa-inverse"></i>
+        </span>
+        <h4 className="my-3">Responsive Design</h4>
+        <p className="text-muted">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
+          maxime quam architecto quo inventore harum ex magni, dicta impedit.
+        </p>
+      </div>
+      <div className="col-md-4">
+        <span className="fa-stack fa-4x">
+          <i className="fas fa-circle fa-stack-2x text-primary"></i>
+          <i className="fas fa-lock fa-stack-1x fa-inverse"></i>
+        </span>
+        <h4 className="my-3">Web Security</h4>
+        <p className="text-muted">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
+          maxime quam architecto quo inventore harum ex magni, dicta impedit.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SingleService;
